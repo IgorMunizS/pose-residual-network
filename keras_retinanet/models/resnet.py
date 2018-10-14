2f7f10d3e00b66f8fa6c555ee2b47e64111cacf3
IgorMunizS <by_skall@hotmail.com>
2018-10-14 12:04:03 -0300

    add keras retinanet for person detection

--- keras_retinanet/models/resnet.py
+++ keras_retinanet/models/resnet.py
@@ -18,11 +18,13 @@ import keras
 from keras.utils import get_file
 import keras_resnet
 import keras_resnet.models
-
+from keras import backend
 from . import retinanet
 from . import Backbone
 from ..utils.image import preprocess_image
-
+from keras import layers
+from keras.layers import Input
+from keras.models import Model
 
 class ResNetBackbone(Backbone):
     """ Describes backbone information and provides utility functions.
@@ -75,6 +77,174 @@ class ResNetBackbone(Backbone):
         return preprocess_image(inputs, mode='caffe')
 
 
+class ResNet():
+    # initialize model from config file
+    def __init__(self, input=None, architecture='resnet50', weight_decay=0.001):
+
+        # hyperparameter config file
+        if input == None:
+            self.input = (None, None, 3)
+        else:
+            self.input = input
+        # create Keras model
+        self.WEIGHT_DECAY = weight_decay
+        self.model = self._create_model(architecture)
+
+    # creates keras model
+    def _create_model(self, architecture):
+
+        assert architecture in ["resnet50", "resnet101"]
+        if backend.image_data_format() == 'channels_last':
+            bn_axis = 3
+        else:
+            bn_axis = 1
+
+        input_image = Input(shape=self.input,
+                            name="input_1")
+        # Stage 1
+
+        x = layers.ZeroPadding2D(padding=(3, 3), name='conv1_pad')(input_image)
+        x = layers.Conv2D(64, (7, 7),
+                          strides=(2, 2),
+                          padding='valid',
+                          kernel_initializer='he_normal',
+                          name='conv1')(x)
+        x = layers.BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+        x = layers.Activation('relu')(x)
+        x = layers.ZeroPadding2D(padding=(1, 1), name='pool1_pad')(x)
+        C1 = x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+        # Stage 2
+        x = self.conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+        x = self.identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+        C2 = x = self.identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+        # Stage 3
+        x = self.conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+        x = self.identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+        x = self.identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+        C3 = x = self.identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+
+        # Stage 4
+        x = self.conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+        x = self.identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+        x = self.identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+        x = self.identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+        x = self.identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+
+        C4 = x = self.identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+        # Stage 5
+        x = self.conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+        x = self.identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+        C5 = x = self.identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+        model = Model(inputs=input_image, outputs=[C2,C3,C4,C5])
+
+        return model
+
+
+
+    def identity_block(self, input_tensor, kernel_size, filters, stage, block):
+        """The identity block is the block that has no conv layer at shortcut.
+        # Arguments
+            input_tensor: input tensor
+            kernel_size: default 3, the kernel size of
+                middle conv layer at main path
+            filters: list of integers, the filters of 3 conv layer at main path
+            stage: integer, current stage label, used for generating layer names
+            block: 'a','b'..., current block label, used for generating layer names
+        # Returns
+            Output tensor for the block.
+        """
+        filters1, filters2, filters3 = filters
+        if backend.image_data_format() == 'channels_last':
+            bn_axis = 3
+        else:
+            bn_axis = 1
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+        x = layers.Conv2D(filters1, (1, 1),
+                          kernel_initializer='he_normal',
+                          name=conv_name_base + '2a')(input_tensor)
+        x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+        x = layers.Activation('relu')(x)
+
+        x = layers.Conv2D(filters2, kernel_size,
+                          padding='same',
+                          kernel_initializer='he_normal',
+                          name=conv_name_base + '2b')(x)
+        x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+        x = layers.Activation('relu')(x)
+
+        x = layers.Conv2D(filters3, (1, 1),
+                          kernel_initializer='he_normal',
+                          name=conv_name_base + '2c')(x)
+        x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+        x = layers.add([x, input_tensor])
+        x = layers.Activation('relu')(x)
+        return x
+
+    def conv_block(self,input_tensor,
+                   kernel_size,
+                   filters,
+                   stage,
+                   block,
+                   strides=(2, 2)):
+        """A block that has a conv layer at shortcut.
+        # Arguments
+            input_tensor: input tensor
+            kernel_size: default 3, the kernel size of
+                middle conv layer at main path
+            filters: list of integers, the filters of 3 conv layer at main path
+            stage: integer, current stage label, used for generating layer names
+            block: 'a','b'..., current block label, used for generating layer names
+            strides: Strides for the first conv layer in the block.
+        # Returns
+            Output tensor for the block.
+        Note that from stage 3,
+        the first conv layer at main path is with strides=(2, 2)
+        And the shortcut should have strides=(2, 2) as well
+        """
+        filters1, filters2, filters3 = filters
+        if backend.image_data_format() == 'channels_last':
+            bn_axis = 3
+        else:
+            bn_axis = 1
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+        x = layers.Conv2D(filters1, (1, 1), strides=strides,
+                          kernel_initializer='he_normal',
+                          name=conv_name_base + '2a')(input_tensor)
+        x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+        x = layers.Activation('relu')(x)
+
+        x = layers.Conv2D(filters2, kernel_size, padding='same',
+                          kernel_initializer='he_normal',
+                          name=conv_name_base + '2b')(x)
+        x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+        x = layers.Activation('relu')(x)
+
+        x = layers.Conv2D(filters3, (1, 1),
+                          kernel_initializer='he_normal',
+                          name=conv_name_base + '2c')(x)
+        x = layers.BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+        shortcut = layers.Conv2D(filters3, (1, 1), strides=strides,
+                                 kernel_initializer='he_normal',
+                                 name=conv_name_base + '1')(input_tensor)
+        shortcut = layers.BatchNormalization(
+            axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+        x = layers.add([x, shortcut])
+        x = layers.Activation('relu')(x)
+        return x
+
+
+
 def resnet_retinanet(num_classes, backbone='resnet50', inputs=None, modifier=None, **kwargs):
     """ Constructs a retinanet model using a resnet backbone.
 
@@ -94,15 +264,7 @@ def resnet_retinanet(num_classes, backbone='resnet50', inputs=None, modifier=Non
         else:
             inputs = keras.layers.Input(shape=(None, None, 3))
 
-    # create the resnet backbone
-    if backbone == 'resnet50':
-        resnet = keras_resnet.models.ResNet50(inputs, include_top=False, freeze_bn=True)
-    elif backbone == 'resnet101':
-        resnet = keras_resnet.models.ResNet101(inputs, include_top=False, freeze_bn=True)
-    elif backbone == 'resnet152':
-        resnet = keras_resnet.models.ResNet152(inputs, include_top=False, freeze_bn=True)
-    else:
-        raise ValueError('Backbone (\'{}\') is invalid.'.format(backbone))
+    resnet = ResNet(inputs).model
 
     # invoke modifier if given
     if modifier:
@@ -112,15 +274,3 @@ def resnet_retinanet(num_classes, backbone='resnet50', inputs=None, modifier=Non
     print(resnet.output)
     # create the full model
     return retinanet.retinanet(inputs=inputs, num_classes=num_classes, backbone_layers=resnet.outputs[1:], **kwargs)
-
-
-def resnet50_retinanet(num_classes, inputs=None, **kwargs):
-    return resnet_retinanet(num_classes=num_classes, backbone='resnet50', inputs=inputs, **kwargs)
-
-
-def resnet101_retinanet(num_classes, inputs=None, **kwargs):
-    return resnet_retinanet(num_classes=num_classes, backbone='resnet101', inputs=inputs, **kwargs)
-
-
-def resnet152_retinanet(num_classes, inputs=None, **kwargs):
-    return resnet_retinanet(num_classes=num_classes, backbone='resnet152', inputs=inputs, **kwargs)
