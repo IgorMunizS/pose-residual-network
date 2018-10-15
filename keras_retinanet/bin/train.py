38c28e24c52ee91cc41480bec4b907f28149c31b
IgorMunizS <by_skall@hotmail.com>
2018-10-15 20:09:00 -0300

    add validation in training

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -483,6 +483,7 @@ def main(args=None):
     training_model.fit_generator(
         generator=train_generator,
         steps_per_epoch=args.steps,
+        validation_data=validation_generator,
         epochs=args.epochs,
         verbose=1,
         callbacks=callbacks,
